b93829a4cde4c02237bc4cdace088138a59c8c01
Anush Kumar <116365814+AnushKumar17@users.noreply.github.com>
2024-02-26 22:51:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use(cors());
+app.use(cors({ credentials: true }));
 // app.use(cors({origin:"*",credentials:true}))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://adwego-frontend.vercel.app');
